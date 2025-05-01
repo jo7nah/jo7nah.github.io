0761600abaf42b25244ff2971d8fd3c8083ad9a4
jonah <142278486+jo7nah@users.noreply.github.com>
2025-05-01 13:04:13 +0200

    added color picker for spotify and started visualizer

--- scripts/spotify_status.js
+++ scripts/spotify_status.js
@@ -1,4 +1,6 @@
 const CURRENTLY_PLAYING_ENDPOINT = "https://jo7nah-portfolio.netlify.app/.netlify/functions/server/spotify/currentlyPlaying";
+const albumCover = document.getElementById('spotify-album-cover');
+const cssRoot = document.querySelector(':root');
 
 function run() {
     getCurrentlyPlaying();
@@ -20,11 +22,19 @@ function getCurrentlyPlaying() {
 }
 
 function onSuccess(response) {
-    document.getElementById("spotify-album-cover").src = JSON.parse(response)['image'];
+    albumCover.src = JSON.parse(response)['image'];
     if(JSON.parse(response)['currentlyPlaying'] == true) {
         console.log("jonah is currently playing music");
         document.getElementById("spotify-status").innerHTML = "Now jamming: ";
         document.getElementById("spotify-text").innerHTML = '<a href="' + JSON.parse(response)['href'] + '">' + JSON.parse(response)['track'] + '</a><span id="spotify-text-by"> by </span><a href="' + JSON.parse(response)['artists_href'] + '">' + JSON.parse(response)['artists'] + '</a>';
+
+        const vibrant = new Vibrant(albumCover);
+        vibrant.getPalette().then(palette => {
+            const color = palette.Vibrant.hex;
+            cssRoot.style.setProperty('--spotify-color', color);
+            console.log('album color is: ' + color);
+            
+        });
     } else {
         console.log("jonah is not playing any music right now");
         document.getElementById("spotify-status").innerHTML = "Recently played: ";
